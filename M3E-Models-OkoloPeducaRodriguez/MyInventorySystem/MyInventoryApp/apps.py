9f4d0cffa5322c5920f0921b80c42a2f3b36448d
chukwunyeluukgabrielokolo-cmyk <chukwunyelu.uk.gabriel.okolo@student.ateneo.edu>
2026-02-13 14:49:33 +0800

    models

--- M3E-Models-OkoloPeducaRodriguez/MyInventorySystem/MyInventoryApp/apps.py
+++ M3E-Models-OkoloPeducaRodriguez/MyInventorySystem/MyInventoryApp/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class MyinventoryappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'MyInventoryApp'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "MyInventoryApp"
--- M3E-Models-OkoloPeducaRodriguez/MyInventorySystem/MyInventoryApp/models.py
+++ M3E-Models-OkoloPeducaRodriguez/MyInventorySystem/MyInventoryApp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Supplier(models.Model):
+    name = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def GetName(self):
+        return self.name
+
+    def __str__(self):
+        f'Supplier: {self.name}, City: {self.city}, Country: {self.country}, Created At: {self.created_at}'
+        
