17d6a2773b7108e4c1c103b134f2a79f4b00be74
karlpeduca <karl.nicholai.peduca@student.ateneo,edu>
2026-02-17 00:46:54 +0800

    Registered models to administrator console

--- M3E-Models-OkoloPeducaRodriguez/MyInventorySystem/MyInventoryApp/admin.py
+++ M3E-Models-OkoloPeducaRodriguez/MyInventorySystem/MyInventoryApp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from.models import Supplier, WaterBottle
+
+@admin.register(Supplier)
+class SupplierAdmin(admin.ModelAdmin):
+    list_display = ('name','city','country','created_at')
+
+@admin.register(WaterBottle)
+class WaterBottleAdmin(admin.ModelAdmin):
+    list_display = ('SKU','brand','cost','size','mouth_size','color','supplied_by','current_quantity')
 
 # Register your models here.
