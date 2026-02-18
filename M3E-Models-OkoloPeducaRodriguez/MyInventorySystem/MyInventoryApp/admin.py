37572a32153cc14b921f231a4d2d509def89bd97
karlpeduca <karl.nicholai.peduca@student.ateneo,edu>
2026-02-18 21:03:30 +0800

    No need for additional UI stuff

--- M3E-Models-OkoloPeducaRodriguez/MyInventorySystem/MyInventoryApp/admin.py
+++ M3E-Models-OkoloPeducaRodriguez/MyInventorySystem/MyInventoryApp/admin.py
@@ -1,12 +1,7 @@
 from django.contrib import admin
 from.models import Supplier, WaterBottle
 
-@admin.register(Supplier)
-class SupplierAdmin(admin.ModelAdmin):
-    list_display = ('name','city','country','created_at')
-
-@admin.register(WaterBottle)
-class WaterBottleAdmin(admin.ModelAdmin):
-    list_display = ('SKU','brand','cost','size','mouth_size','color','supplied_by','current_quantity')
+admin.site.register(Supplier)
+admin.site.register(WaterBottle)
 
 # Register your models here.
